e712132b4017b617ec76c6fe49a151e7884f6e35
Panagiotis Papadopoulos <panagiotpapa@gmail.com>
2023-04-01 14:28:40 +0300

    Complete patch route for order history

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -16,19 +16,13 @@ userRouter.post("/login", User.login);
 //Find and return a user that already exists based on his id
 userRouter.get("/user/:userId", User.getOneUser)
 
-//Find and return a user that already exists based on his username
-userRouter.get("/user/username/:username", User.getOneUserByUsername)
-
-//Find a user with a specific username and return his orderHistory list
-userRouter.get("/user/orderhistory/:username", User.getOneUserByUsernameAndReturnOrderHistory)
-
 //Find a user with a specific username and return his orderHistory list
 userRouter.get("/user/orderhistoryById/:userId", User.getOneUserByIdAndReturnOrderHistory)
 
-//TODO : Add a post endpoint to add an event to a user's order history 
+//Add an event to a user's order history 
+userRouter.patch("/user/addordertohistory/:userId/:eventId", User.addOrderToHistory)
 
-userRouter.patch("/user/addordertohistory/:userId/eventId", User.addOrderToHistory)
-
+//Delete a user's profile
 userRouter.delete("/user/delete/:userId",User.deleteUser )
 
 
