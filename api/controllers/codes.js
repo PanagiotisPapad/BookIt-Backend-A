fe6990e5ea9142e4ba40be3e32012e538d1c76cd
Vaggos B <37471733+VaggosBt@users.noreply.github.com>
2023-04-07 01:25:54 +0300

    Change getDiscountByCode to return json percentage in a specific respond way

--- api/controllers/codes.js
+++ api/controllers/codes.js
@@ -30,7 +30,7 @@ exports.getDiscountByCode = async (req, res) => {
       const code = await Code.findOne({codeName: codeName});
   
       if (code) {
-        res.send(code.discountPercentage.toString());
+        res.json({ "discount": code.discountPercentage.toString() });
       } else {
         res.status(404).json({
           message: "The coupon you entered is not valid"
